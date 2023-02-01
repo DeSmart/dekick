c2aa2488916173d864f165f98e4fe9e547690cae
Piotr Duszyński <dooshek@desmart.com>
2023-02-01 16:09:02 +0100

    feat: ability to add path to .env file

--- lib/dotenv.py
+++ lib/dotenv.py
@@ -3,13 +3,19 @@ from dotenv import dotenv_values
 from lib.settings import C_CMD, C_END, C_FILE
 
 
-def get_dotenv_var(var: str, default: str = "") -> str:
+def get_dotenv_var(var: str, default: str = "", path: str = "") -> str:
     """Gets variable var from .env file"""
-    config = dotenv_values(".env")
+
+    path_parsed = path
+
+    if not path.endswith("/"):
+        path_parsed = f"{path}/"
+
+    config = dotenv_values(f"{path_parsed}.env")
 
     if var not in config and default == "":
         raise KeyError(
-            f"Variable {C_CMD}{var}{C_END} not found in {C_FILE}.env{C_END} file"
+            f"Variable {C_CMD}{var}{C_END} not found in {C_FILE}{path}.env{C_END} file"
         )
 
     if var not in config and default != "":
