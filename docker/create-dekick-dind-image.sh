abde080cf5ea1c8859b7ea7ae00a35bcf7194116
Piotr Duszyński <dooshek@desmart.com>
2023-02-15 13:24:38 +0100

    fix: wrong paths

--- docker/create-dekick-dind-image.sh
+++ docker/create-dekick-dind-image.sh
@@ -4,9 +4,9 @@ BASEIMAGE="desmart/dekick-dind"
 TAG=$(cat ../.version)
 IMAGE="${BASEIMAGE}:${TAG}"
 
-cp ../requirements.txt dekick/
+cp ../requirements.txt dind/
 docker run --privileged --rm tonistiigi/binfmt --install all
 docker buildx create --use --name multiarch
 docker buildx inspect --bootstrap
 docker buildx build --pull --platform="linux/amd64,linux/arm64/v8" --tag "${IMAGE}" --push dind/
-rm requirements.txt
+rm dind/requirements.txt
--- docker/create-dekick-image.sh
+++ docker/create-dekick-image.sh
@@ -4,9 +4,9 @@ BASEIMAGE="desmart/dekick"
 TAG=$(cat ../.version)
 IMAGE="${BASEIMAGE}:${TAG}"
 
-cp ../requirements.txt dind/
+cp ../requirements.txt dekick/
 docker run --privileged --rm tonistiigi/binfmt --install all
 docker buildx create --use --name multiarch
 docker buildx inspect --bootstrap
-docker buildx build --pull --platform="linux/amd64,linux/arm64/v8" --tag "${IMAGE}" --push dind/
-rm requirements.txt
+docker buildx build --pull --platform="linux/amd64,linux/arm64/v8" --tag "${IMAGE}" --push dekick/
+rm dekick/requirements.txt
