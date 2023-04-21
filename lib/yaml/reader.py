7ceac448f2b0c41f627d8c40d7f05bfce9a524b5
Piotr Duszyński <dooshek@desmart.com>
2023-04-21 13:53:53 +0200

    refactor: moved to lib.yaml

--- lib/yaml/reader.py
+++ lib/yaml/reader.py
@@ -1,3 +1,4 @@
+"""Read YAML files as flatdict"""
 import sys
 from os import path
 
@@ -7,13 +8,13 @@ import yaml
 from lib.settings import C_END, C_FILE
 
 
-def get_yaml_flat(file) -> flatdict.FlatDict:
+def read_yaml(file) -> flatdict.FlatDict:
     """Get flattened YAML file"""
     if not path.exists(file):
         print(f"File {C_FILE}{file}{C_END} does not exists")
         sys.exit(1)
 
-    with (open(f"{file}", "r", encoding="utf-8")) as yaml_file:
+    with (open(file, "r", encoding="utf-8")) as yaml_file:
         yaml_parsed = yaml.safe_load(yaml_file)
         ret = flatdict.FlatDict(yaml_parsed, delimiter=".")
         return ret
---
+++ lib/yaml/saver.py
@@ -0,0 +1,9 @@
+"""Flat to yaml saver"""
+import flatdict
+from yaml import dump
+
+
+def save_flat(filename: str, flat: flatdict.FlatDict):
+    """Saves flat dict to .dekickrc.yml file"""
+    with open(filename, "w", encoding="utf-8") as yaml_file:
+        dump(flat.as_dict(), yaml_file)
