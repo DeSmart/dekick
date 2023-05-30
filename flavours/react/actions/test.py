5c7e9327ce223fac47aa43298711de4fdd9c089b
Piotr Duszyński <dooshek@desmart.com>
2023-05-30 13:50:50 +0200

    feat: added tests for react flavour

---
+++ flavours/react/actions/test.py
@@ -0,0 +1,23 @@
+"""
+Local run for Node ExpressJS (backend) application
+"""
+from rich.console import Console
+from rich.traceback import install
+
+from commands.yarn import ui_yarn
+from flavours.shared import (
+    copy_artifacts_from_dind,
+    pull_and_build_images,
+    yarn_install,
+)
+
+install()
+console = Console()
+
+
+def main():
+    """Main"""
+    pull_and_build_images()
+    yarn_install()
+    copy_artifacts_from_dind()
+    ui_yarn(args=["test", "--ci", "--watchAll=false", "--forceExit"])
