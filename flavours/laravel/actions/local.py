6527165675f100fbfe34a3e399ce4b8b46787a39
Piotr Duszyński <dooshek@desmart.com>
2023-06-12 10:59:33 +0200

    feat(laravel): speed up dekick local execution by removing setting permissions

--- flavours/laravel/actions/local.py
+++ flavours/laravel/actions/local.py
@@ -1,32 +1,21 @@
 """
 Local run for Laravel (backend) application
 """
-from rich.console import Console
 from rich.traceback import install
 
 from commands.seed import ui_seed
-from flavours.laravel.shared import (
-    api_is_ready,
-    fix_permissions,
-    generate_apidoc,
-    laravel_nova_support,
-    setup_permissions,
-)
+from flavours.laravel.shared import api_is_ready, generate_apidoc, laravel_nova_support
 from flavours.shared import composer_install, pull_and_build_images, start_services
 
 install()
-console = Console()
 
 
 def main():
     """Main"""
-
     laravel_nova_support()
     pull_and_build_images()
-    setup_permissions()
     composer_install()
     start_services()
-    fix_permissions()
     ui_seed()
     generate_apidoc()
     api_is_ready()
